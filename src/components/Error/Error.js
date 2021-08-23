4cf84593e76ae882f191cc2ae6b2852cc212f440
John Gillan <jgillan@protonmail.com>
2021-08-23 16:23:20 +0100

    add backup error to Error component

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,10 +1,12 @@
 import './Error.scss';
 
 const Error = ({ message }) => {
+  const displayedMessage = message || 'An unexpected error occured';
+
   return (
     <div className="error">
       <h1>There was a problem:</h1>
-      <h2>{message}</h2>;<h1>Please try again later</h1>
+      <h2>{displayedMessage}</h2>;<h1>Please try again later</h1>
     </div>
   );
 };
--- src/components/Error/Error.test.js
+++ src/components/Error/Error.test.js
@@ -1,9 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import Error from './Error';
 
-test('Displays error recieved from props', () => {
-  render(<Error message={'There was an error'} />);
+describe('Error component', () => {
+  test('displays error recieved from props', () => {
+    render(<Error message={'There was an error'} />);
 
-  const errorTest = screen.getByText('There was an error');
-  expect(errorTest).toBeInTheDocument();
+    const errorTest = screen.getByText('There was an error');
+    expect(errorTest).toBeInTheDocument();
+  });
+
+  test('displays backup error if props are falsey', () => {
+    render(<Error message={''} />);
+
+    const errorTest = screen.getByText('An unexpected error occured');
+    expect(errorTest).toBeInTheDocument();
+  });
 });
